4106402c59a490fe41d3946c590740f36aa5006e
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-09-10 13:56:31 +0200

    Updated (cleaned) setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.mount("#app");
